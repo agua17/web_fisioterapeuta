d7bdb4446d90768039c8933633de7ec811046582
Agua <aguassantasnunez@gmail.com>
2024-11-27 18:53:41 +0100

    configuracion tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { HomeComponent } from './pages/home/home/home.component';
 import { Routes } from '@angular/router';
+import path from 'node:path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: ' ', component: HomeComponent},
+
+];
